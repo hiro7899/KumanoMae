6dcf61543e32568236001e95ad996f54da76855f
hiro7899 <mihaya1016@gmail.com>
2026-09-01 14:51:19 +0900

    Fix: resources通過のための修正

--- src/main/java/com/jsl/util/EncodingFilter.java
+++ src/main/java/com/jsl/util/EncodingFilter.java
@@ -14,8 +14,6 @@ public class EncodingFilter implements Filter {
             throws IOException, ServletException {
         
         request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        
         chain.doFilter(request, response);
     }
 }
