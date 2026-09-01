8fbf8d9c004c139ef950539b6f948267a850f07f
hiro7899 <mihaya1016@gmail.com>
2026-09-01 18:53:17 +0900

    Feat: PasswordUtilを追加して会員登録処理を修正

--- src/main/java/com/jsl/service/login/SignUpService.java
+++ src/main/java/com/jsl/service/login/SignUpService.java
@@ -8,28 +8,27 @@ import javax.servlet.http.HttpServletResponse;
 import com.jsl.dao.AuthDao;
 import com.jsl.dto.member.MemberDto;
 import com.jsl.service.Command;
+import com.jsl.util.PasswordUtil;
 
-public class SignUpService implements Command{
+public class SignUpService implements Command {
 
-	@Override
-	public void doCommand(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doCommand(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		MemberDto member = new MemberDto();
-		member.setUserId(request.getParameter("userId"));
-		member.setUserPw(request.getParameter("userPw"));
-		member.setUserName(request.getParameter("userName"));
-		member.setEmail(request.getParameter("email"));
-		
-		if(request.getParameter("phone") != null && !request.getParameter("phone").isEmpty()) {
-		    member.setPhone(request.getParameter("phone"));
-		} else {
-		    member.setPhone(null);
-		}
-		
-		AuthDao dao = new AuthDao();
-		
-		dao.SignUp(member);
-		
-	}
+        MemberDto member = new MemberDto();
 
+        member.setUserId(request.getParameter("userId"));
+
+        String userPw = request.getParameter("userPw");
+        member.setUserPw(PasswordUtil.hashPassword(userPw));
+
+        member.setUserName(request.getParameter("userName"));
+        member.setEmail(request.getParameter("email"));
+
+        String phone = request.getParameter("phone");
+        member.setPhone(phone != null && !phone.isEmpty() ? phone : null);
+
+        AuthDao dao = new AuthDao();
+        dao.SignUp(member);
+    }
 }
